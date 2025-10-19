fbcd9b88f2599f9ad95f95302de87a17f46e771a
Stanley Osagie Ramos <sagiecyber@gmail.com>
2025-10-20 00:45:36 +0100

    notification.rs - Notification Models

--- vaultless-core/src/models/mod.rs
+++ vaultless-core/src/models/mod.rs
@@ -4,6 +4,7 @@ pub mod message;
 pub mod proof;
 pub mod usage;
 pub mod usage_timescale;
+pub mod notification;
 
 pub use api_key::{ApiKey, CreateApiKey};
 pub use auth::{RefreshToken, User, UserSession};
@@ -11,3 +12,4 @@ pub use message::{CreateMessage, Message, MessageMetadata};
 pub use proof::{CreateProof, MessageProof, ProofVerificationResult, VerifyProofRequest};
 pub use usage::{UsageMetric, UsageSummary};
 pub use usage_timescale::{DailyUsageSummary, MonthlyTotal, UsageTrends, WeeklyUsageSummary};
+pub use notification::*;
