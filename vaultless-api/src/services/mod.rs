31061e1d49b17f48adfd19555629ddb227e38c59
Stanley Osagie Ramos <sagiecyber@gmail.com>
2025-10-20 13:09:04 +0100

    notification_service.rs - Background Jobs

--- vaultless-api/src/services/mod.rs
+++ vaultless-api/src/services/mod.rs
@@ -2,6 +2,8 @@ pub mod analytics;
 pub mod cache;
 pub mod rate_limiter;
 pub mod token;
+pub mod notification_job;
 
 pub use cache::CacheService;
 pub use rate_limiter::RateLimiter;
+pub use notification_job::*;
